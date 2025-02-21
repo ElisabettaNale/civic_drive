3d887f9c7fc786f88fd5ff61012215e0384651be
ElisabettaNale <elisabetta.nale@gmail.com>
2025-02-21 12:41:01 +0100

    render mode dynamic

--- netlify/functions/ssr-function.ts
+++ netlify/functions/ssr-function.ts
@@ -1,10 +1,36 @@
 import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import { getContext } from '@netlify/angular-runtime/context';
+import express from 'express';
 
 const angularAppEngine = new AngularAppEngine();
+const app = express();
+
+app.use(express.static('dist/browser'));
+
+app.use('/users-list/:id', async (req, res) => {
+  const renderMode = 'dynamic'; 
+
+  const requestForNetlify = new Request(req.url, {
+    method: req.method,
+    headers: req.headers as any,
+    body: req.body,
+  });
+
+  try {
+    const response = await angularAppEngine.handle(requestForNetlify);
+
+    if (response) {
+      res.send(response);
+    } else {
+      res.status(404).send('Not found');
+    }
+  } catch (error) {
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext(); 
+  const context = getContext();
   const result = await angularAppEngine.handle(request, context);
   return result || new Response('Not found', { status: 404 });
 }
--- src/server.ts
+++ src/server.ts
@@ -2,28 +2,25 @@ import { AngularAppEngine, createRequestHandler } from '@angular/ssr';
 import { getContext } from '@netlify/angular-runtime/context';
 import express from 'express';
 
-const angularAppEngine = new AngularAppEngine();  
+const angularAppEngine = new AngularAppEngine();
 const app = express();
-
 
 app.use(express.static('dist/browser'));
 
-
 app.use('/users-list/:id', async (req, res) => {
-  const renderMode = 'full';  
+  const renderMode = 'dynamic';  
 
   const requestForNetlify = new Request(req.url, {
     method: req.method,
-    headers: req.headers as any,  
-    body: req.body,  
+    headers: req.headers as any,
+    body: req.body,
   });
 
   try {
-    
     const response = await angularAppEngine.handle(requestForNetlify);
 
     if (response) {
-      res.send(response);  
+      res.send(response);
     } else {
       res.status(404).send('Not found');
     }
@@ -33,11 +30,9 @@ app.use('/users-list/:id', async (req, res) => {
 });
 
 export async function netlifyAppEngineHandler(request: Request): Promise<Response> {
-  const context = getContext();  
-  const result = await angularAppEngine.handle(request, context);  
-  return result || new Response('Not found', { status: 404 }); 
+  const context = getContext();
+  const result = await angularAppEngine.handle(request, context);
+  return result || new Response('Not found', { status: 404 });
 }
 
-export const reqHandler = createRequestHandler(netlifyAppEngineHandler);  
-
-
+export const reqHandler = createRequestHandler(netlifyAppEngineHandler);
